2c924920d2e58b74c8bb2a057c38cde673f98e2b
Kachy <ejimovc@gmail.com>
2024-06-21 05:01:34 +0000

    The db storage task done, First page rendered with flask

---
+++ hie_flask/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+ 
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def landing_page():
+    """ Displays the landing page """
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
