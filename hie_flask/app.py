88f73b1799ea8a293d6215ed8787b69881f97298
Kachy <ejimovc@gmail.com>
2024-06-22 08:24:25 +0000

    Updating the main branch with the correct file structure

--- hie_flask/app.py
+++ hie_flask/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def landing_page():
     """ Displays the landing page """
-    return render_template('hie.html')
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
