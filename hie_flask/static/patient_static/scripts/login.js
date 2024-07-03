61e560ccaad6de02ddb26d88957253f4b4609bae
Kachy <ejimovc@gmail.com>
2024-07-03 14:14:54 +0000

    upload

--- hie_flask/static/patient_static/scripts/login.js
+++ hie_flask/static/patient_static/scripts/login.js
@@ -1,74 +1,89 @@
-$(document).ready(function() {
-    const form = $('#signupForm');
-    const errorMessage = $('#error-message');
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('signupForm');
+    const errorMessage = document.getElementById('error-message');
     const baseURL = 'https://52.91.118.78';
 
-    // Function to show error message
+        // Function to show error message
     function showError(input, message) {
-        const error = $('<div>').addClass('error-message text-danger').text(message);
-        input.parent().append(error);
+            const error = document.createElement('div');
+            error.className = 'error-message text-danger';
+            error.innerText = message;
+            input.parentNode.appendChild(error);
+    
+            // Remove error message when input is valid
+            input.addEventListener('input', function() {
+                error.remove();
+            });
+        }
 
-        // Remove error message when input is valid
-        input.on('input', function() {
-            error.remove();
-        });
+        async function validateUser(email, password) {
+            const response = await fetch(`${baseURL}:5001/hie_api/v1/validate`, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Request-Version': 'HTTP/1.1',
+                    'Secure': 'true',
+                    'SameSite': 'None',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    user: 'patient',
+                    opp: 'login'
+                })
+            });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        return response.json();
     }
-
-    function validateUser(email, password) {
-        return $.ajax({
-            url: `${baseURL}:5001/hie_api/v1/validate`,
-            type: 'POST',
-            headers: {
-                'Request-Version': 'HTTP/1.1',
-                'Secure': 'true',
-                'SameSite': 'None',
-                'Content-Type': 'application/json'
-            },
-            data: JSON.stringify({
-                email: email,
-                password: password,
-                user: 'patient',
-                opp: 'login'
-            }),
-            dataType: 'json'
-        });
-    }
-
-    form.on('submit', function(event) {
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
         let isValid = true;
-        errorMessage.html('');
+        errorMessage.innerHTML = '';
 
         // Clear previous error messages
-        $('.error-message').remove();
+        const errorElements = document.querySelectorAll('.error-message');
+        errorElements.forEach(element => element.remove());
 
         // Validate each field
         const fields = ['email', 'password'];
         fields.forEach(field => {
-            const input = $('#' + field);
-            if (!input.val() || (input.prop('type') === 'select-one' && input.val() === 'Default')) {
+            const input = document.getElementById(field);
+            if (!input.value || (input.type === 'select-one' && input.value === 'Default')) {
                 isValid = false;
                 showError(input, 'This field cannot be empty');
             }
         });
 
-        const password = $('#password');
-        const email = $('#email');
 
+        const password = document.getElementById('password');
+
+        const email = document.getElementById('email');
         if (isValid) {
-            validateUser(email.val(), password.val()).done(function(response) {
+            // Perform the additional check before submitting the form
+            validateUser(email.value, password.value).then(response => {
                 if (!response.email) {
-                    showError(email, 'User not found');
-                    errorMessage.html(`It looks like you don't have an account, no worries <a href="${baseURL}:5000/patient/signup">Sign up here!</a>`);
+                    // Display the login page link
+                    showError(email, `User not found`);
+                    errorMessage.innerHTML = `It looks like you don't have an account, no worries <a href="${baseURL}:5000/patient/signup">Sign up here!</a>`;
                 } else if (!response.password) {
-                    showError(password, 'Incorrect password.');
+                    // Display the login page link
+                    showError(password, `Incorrect password.`);
                 } else {
+                    // Submit the form if all validations pass
                     form.submit();
                 }
-            }).fail(function(error) {
+            }).catch(error => {
+
                 console.error('Error validating user:', error);
-                errorMessage.html('An error occurred while validating the user.' + error);
+                errorMessage.innerHTML = 'An error occurred while validating the user.' + error;
             });
         }
     });
+
+
 });
